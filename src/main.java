d1d36f48081eb4f909dfd49877159bf2fe3ec8f6
willrothschild <rothwg21@wfu.edu>
2021-11-29 14:59:34 -0500

    stdDraw added

--- src/main.java
+++ src/main.java
@@ -8,17 +8,34 @@ public class main {
 
         Balance userBal = new Balance(100);
         int[][] slotMachine = new int[3][3];
+        //8 items
         Scanner scnr = new Scanner(System.in);
         boolean check = true;
         int betAmount;
 
         while(check = true){
-            System.out.print("Input bet amount");
+            System.out.print("Input bet amount: ");
             betAmount = scnr.nextInt();
             userBal.setBetAmount(betAmount);
-            System.out.print("Press any key to Spin");
+            System.out.println("Press P to Spin or Q to quit");
             //the on key press code here
+            StdDraw.pause(500);
+            if(StdDraw.isKeyPressed(80)){
 
+                for(int i = 0; i < 3; i++){
+                    for(int j = 0; j < 3; j++){
+                        slotMachine[i][j] = randomNumBetween0And7();
+                        System.out.print(slotMachine[i][j] + ", ");
+                    }
+                }
+
+
+
+
+
+            } else if(StdDraw.isKeyPressed(81)){
+                check = false;
+            }
 
 
 
@@ -27,15 +44,18 @@ public class main {
 
 
         }
+        System.out.println("Your final balance was " + userBal);
 
 
 
 
-        System.out.println("1");
     }
 
 
-
+    public static int randomNumBetween0And7() {
+        Random rand = new Random();
+        return rand.nextInt((7) + 1);
+    }
 
 
 
