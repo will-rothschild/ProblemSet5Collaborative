8f26878126195c7c713de2d895a5547abc611936
willrothschild <rothwg21@wfu.edu>
2021-11-29 14:35:41 -0500

    stdDraw added

--- src/main.java
+++ src/main.java
@@ -9,6 +9,26 @@ public class main {
         Balance userBal = new Balance(100);
         int[][] slotMachine = new int[3][3];
         Scanner scnr = new Scanner(System.in);
+        boolean check = true;
+        int betAmount;
+
+        while(check = true){
+            System.out.print("Input bet amount");
+            betAmount = scnr.nextInt();
+            userBal.setBetAmount(betAmount);
+            System.out.print("Press any key to Spin");
+            //the on key press code here
+
+
+
+
+
+
+
+
+        }
+
+
 
 
         System.out.println("1");
