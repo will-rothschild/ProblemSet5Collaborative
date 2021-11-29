78bdc747d3174c2b98bc53f185dcbbf8c73670f8
willrothschild <rothwg21@wfu.edu>
2021-11-29 15:08:30 -0500

    stdDraw added

--- src/main.java
+++ src/main.java
@@ -14,19 +14,21 @@ public class main {
         int betAmount;
 
         while(check = true){
-            System.out.print("Input bet amount: ");
-            betAmount = scnr.nextInt();
-            userBal.setBetAmount(betAmount);
+
             System.out.println("Press P to Spin or Q to quit");
             //the on key press code here
             StdDraw.pause(500);
             if(StdDraw.isKeyPressed(80)){
+                System.out.print("Input bet amount: ");
+                betAmount = scnr.nextInt();
+                userBal.setBetAmount(betAmount);
 
                 for(int i = 0; i < 3; i++){
                     for(int j = 0; j < 3; j++){
                         slotMachine[i][j] = randomNumBetween0And7();
                         System.out.print(slotMachine[i][j] + ", ");
                     }
+                    System.out.println();
                 }
 
 
